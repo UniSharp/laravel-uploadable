6ce7621f8c03c739cbe407aef9a5c2f9cd113cf3
昱行 <laiyushing@unisharp.com>
2018-01-30 11:37:44 +0800

    add url path of file

--- src/File.php
+++ src/File.php
@@ -8,8 +8,15 @@ class File extends Model
 {
     protected $guarded = [];
 
+    protected $appends = ['url_path'];
+
     public function uploadable()
     {
         return $this->morphTo();
+    }
+
+    public function getUrlPathAttribute()
+    {
+        return url($this->path);
     }
 }
